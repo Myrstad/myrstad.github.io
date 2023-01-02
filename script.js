0881667c85692d03b1c1b0a8950c65648c749e3f
Myrstad <mortenmyrstad@gmail.com>
2023-01-02 22:29:43 +0100

    refinements and navbar functionality

--- script.js
+++ script.js
@@ -10,3 +10,29 @@ const navigationToggle = document.querySelector('#responsive-toggle');
 navigationToggle.addEventListener('click', (e) => {
   navigationToggle.classList.toggle('active');
 });
+
+//get current section and highlight the correct link in the navbar
+const hero = document.querySelector('#intro');
+const projects = document.querySelector('#prosjektene-mine');
+const about = document.querySelector('#om-meg');
+const sections = [hero, projects, about];
+const navbarLinks = document.querySelectorAll('nav ul li a');
+const navbarHeight = 96;
+console.log(sections, navbarLinks);
+window.addEventListener('scroll', (e) => {
+  const scroll = window.scrollY + navbarHeight;
+  sections.forEach((element, index) => {
+    if (element.offsetTop <= scroll) {
+      navbarLinks.forEach((link) => {
+        link.classList.remove('active');
+      });
+      navbarLinks[index].classList.add('active');
+    }
+  });
+  console.log(
+    hero.offsetTop,
+    projects.offsetTop,
+    about.offsetTop,
+    window.scrollY + navbarHeight
+  );
+});
