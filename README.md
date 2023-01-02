d0214e940d746b933b848871e098403593f5df84
Myrstad <mortenmyrstad@gmail.com>
2023-01-02 20:34:37 +0100

    Made the navbar responsive

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # myrstad.github.io
+
+## Min personlige nettside
+
+I alle fall frem til jeg blir 18 og kan kjøpe eget domene :)
--- script.js
+++ script.js
@@ -1,5 +1,12 @@
+//navbar functionality on scroll
 document.addEventListener('scroll', () => {
   if (window.scrollY > 50)
     document.querySelector('.hero').classList.add('scrolled');
   else document.querySelector('.hero').classList.remove('scrolled');
+});
+
+//navbar mobile responisvess
+const navigationToggle = document.querySelector('#responsive-toggle');
+navigationToggle.addEventListener('click', (e) => {
+  navigationToggle.classList.toggle('active');
 });
