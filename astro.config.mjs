c5cbd0c226834f248e7f4f95f8e0f4ed755eddc5
Morten Andreas Myrstad <mortenam@uia.no>
2026-09-13 21:38:51 +0200

    ✨ Added fonts
    Used astro font API, with the google font provider. Which works great. BaseLayout now uses correct fonts via global.css

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,22 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig, fontProviders } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+    fonts: [
+        {
+            name: "Playfair Display SC",
+            cssVariable: "--font-display",
+            provider: fontProviders.google(),
+            weights: [400],
+            styles: ["normal"]
+        },
+        {
+            name: "Source Serif 4",
+            cssVariable: "--font-body",
+            provider: fontProviders.google(),
+            weights: [400],
+            styles: ["normal", "italic"]
+        }
+    ]
+});
